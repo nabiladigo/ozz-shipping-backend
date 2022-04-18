1f5e7ea0d23278276e95450dfef9282148a2af83
nabiladigo <nabiladigo91@gmail.com>
2022-04-18 15:16:08 -0400

    create package routes

--- controllers/package_conrtoller.js
+++ controllers/package_conrtoller.js
@@ -11,3 +11,43 @@ router.get('/', async(req, res)=>{
         res.status(400).json(err);
     }
 });
+router.get('/:id', async(req, res)=>{
+    try{
+        res.json(await Package.findOne({"_id": req.params.id}));
+    }catch(err){
+        res.status(400).json(err);
+    }
+});
+router.post('/', async(req, res)=> {
+    try{
+        res.json(await Package.create(req.bosy));
+    }catch(err){
+        res.status(400).json(err);
+    }
+});
+router.put('/edit/:id', async(req, res)=> {
+    try{
+        const{image, price, title, weight} = req.body;
+        const package = await Package.findByIdAndUpdate(req.params.id, 
+            {
+                $set: req.body,
+            },
+            {new: true,
+            }
+        );
+        const editPackage = packege.save();
+        return res.status(200).json(editPackage);
+    }catch(err){
+        return res.status(500).json(err);
+    }
+});
+router.delete('/delete/:id', async(req, res)=>{
+    try{
+        res.json( await Package.findByIdAndRemove(req.params.id));
+    }catch(err){
+        res.status(400).json(err);
+    }
+});
+
+
+module.exports =router;
